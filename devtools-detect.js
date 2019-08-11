799012252c97261e6442332582dce926c1a52785
Jacob Kuuhaku <jcoflooder123@gmail.com>
2019-08-11 13:09:47 -0300

    v1.1

--- devtools-detect.js
+++ devtools-detect.js
@@ -19,15 +19,6 @@
 				console.log(element);
 				//console.clear();
 
-				if(navigator.userAgent.toLowerCase().indexOf('firefox') == -1){//for exclude scripts on firefox
-					var r = function(){};
-					r.toString = function(){
-						checkStatus = true;
-						//setStatus(true);
-					};
-					console.log("%c", r, "");
-				}
-
 				var widthThreshold = window.outerWidth - window.innerWidth > 160;
 				var heightThreshold = window.outerHeight - window.innerHeight > 160;
 
@@ -37,6 +28,15 @@
 				}
 
 				if(!checkStatus){
+					if(navigator.userAgent.toLowerCase().indexOf('firefox') == -1){//for exclude scripts on firefox
+						var r = function(){};
+						r.toString = function(){
+							checkStatus = true;
+							//setStatus(true);
+						};
+						console.log("%c", r, "");
+					}
+
 					var before = new Date().getTime();
 					eval(function(p,a,c,k,e,r){e=String;if(!''.replace(/^/,String)){while(c--)r[c]=k[c]||c;k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('0;',2,1,'debugger'.split('|'),0,{}));
 					var after = new Date().getTime();
